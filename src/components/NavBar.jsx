3e8440b0a8f4369da3c603fc86ce69f8d248e909
VitaliiRomanenko <vitalii.o.romanenko@gmail.com>
2025-06-23 22:03:57 +0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+function NavBar () {
+
+    return <nav className="navbar">
+        <div className="navbar-brand">
+            <Link to="/">Movie plate</Link>
+        </div>
+        <div className="navbar-links">
+            <Link to="/" className="nav-links">Home</Link>
+            <Link to="/favorites" className="nav-links">Favorites</Link>
+        </div>
+    </nav>
+}
+
+export default NavBar
